df3dca08cc946de7084322356cebcbb1ba405577
sdev26 <67799721+sdev26@users.noreply.github.com>
2026-07-31 20:32:09 +0530

    Time: 17 ms (94.67%) | Memory: 46.9 MB (66.51%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n=s.length();
+        if(n==1)
+            return 0;
+        int dp[][]=new int [n][n];
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=dp[i+1][j-1];
+                }
+                else
+                    dp[i][j]=1+Math.min(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
+    }
+}
